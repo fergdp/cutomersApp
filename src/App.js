64c3b753165a410f0023ea529dadd37e284b41d5
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2019-09-15 19:55:08 -0300

    158 - Componente Link, instalacion de react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { link, BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/customers">Customers</Link> <br></br>
+        <Link to="/customers/30242306">Customer 30242306</Link>
+      </div>
+    </Router>
   );
 }
 
