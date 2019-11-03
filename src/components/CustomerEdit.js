f1adbd39e8f7a0b473d0d118855b06a5927779a1
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2019-11-03 17:48:17 -0300

    194 - Validaciones Multiples en fields y mejora a MyField

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -7,53 +7,55 @@ const isRequired = value => (
     !value && "Este campo es requerido"
 );
 
-const MyField = ({ input, meta }) => (
+const isNumber = value => (
+    isNaN(Number(value)) && "El campo debe ser un numero"
+);
+
+const MyField = ({ input, meta, type, label, name }) => (
     <div>
-        <input {...input} type="text" />
+        <label htmlFor={name}>{label}</label>
+        <input {...input} type={!type ? "text" : type} />
         {
             meta.touched && meta.error && <span>{meta.error}</span>
         }
     </div>
 );
-        
-const CustomerEdit = ({name, dni, age}) => {
+
+const CustomerEdit = ({ name, dni, age }) => {
     return (
         <div>
             <h2>Edición del cliente</h2>
             <form action="">
-                <div>
-                    <label htmlFor="name">Nombre</label>
-                    <Field
-                        name="name"
-                        component={MyField}
-                        type="text"
-                        validate={isRequired}>
-                    </Field>
-                </div>
-                <div>
-                    <label htmlFor="dni">Dni</label>
-                    <Field 
-                        name="dni" 
-                        component={MyField}
-                        type="text"
-                        validate={isRequired}>
-                        </Field>
-                </div>
-                <div>
-                    <label htmlFor="age">Edad</label>
-                    <Field name="age" component="input" type="number"></Field>
-                </div>
+                <Field
+                    name="name"
+                    component={MyField}
+                    validate={isRequired}
+                    label="Nombre">
+                </Field>
+                <Field
+                    name="dni"
+                    component={MyField}
+                    validate={[isRequired, isNumber]}
+                    label="Dni">
+                </Field>
+                <Field
+                    name="age"
+                    component={MyField}
+                    type="number"
+                    validate={isNumber}
+                    label="Edad">
+                </Field>
             </form>
         </div>
-        );
-    };
-    
+    );
+};
+
 CustomerEdit.propTypes = {
-            name: PropTypes.string,
-        dni: PropTypes.string,
-        age: PropTypes.number,
-    };
-    
-const CustomerEditForm = reduxForm({form: 'CustomerEdit' })(CustomerEdit);
-        
+    name: PropTypes.string,
+    dni: PropTypes.string,
+    age: PropTypes.number,
+};
+
+const CustomerEditForm = reduxForm({ form: 'CustomerEdit' })(CustomerEdit);
+
 export default setPropsAsInitial(CustomerEditForm);
