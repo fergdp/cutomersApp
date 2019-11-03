4dea6070f95a51628ab39bbd4f2e36b75764fd92
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2019-11-03 15:57:25 -0300

    191 y 193 Validacion a nivel de field (isRequired)

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
-import { connect } from 'react-redux';
+import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 
-const CustomerEdit = ({ name, dni, age }) => {
+const isRequired = value => (
+    !value && "Este campo es requerido"
+);
+
+const MyField = ({ input, meta }) => (
+    <div>
+        <input {...input} type="text" />
+        {
+            meta.touched && meta.error && <span>{meta.error}</span>
+        }
+    </div>
+);
+        
+const CustomerEdit = ({name, dni, age}) => {
     return (
         <div>
             <h2>Edición del cliente</h2>
             <form action="">
                 <div>
                     <label htmlFor="name">Nombre</label>
-                    <Field name="name" component="input" type="text"></Field>
+                    <Field
+                        name="name"
+                        component={MyField}
+                        type="text"
+                        validate={isRequired}>
+                    </Field>
                 </div>
                 <div>
                     <label htmlFor="dni">Dni</label>
-                    <Field name="dni" component="input" type="text"></Field>
+                    <Field 
+                        name="dni" 
+                        component={MyField}
+                        type="text"
+                        validate={isRequired}>
+                        </Field>
                 </div>
                 <div>
                     <label htmlFor="age">Edad</label>
@@ -22,18 +45,15 @@ const CustomerEdit = ({ name, dni, age }) => {
                 </div>
             </form>
         </div>
-    );
-};
-
+        );
+    };
+    
 CustomerEdit.propTypes = {
-    name: PropTypes.string,
-    dni: PropTypes.string,
-    age: PropTypes.number,
-};
-
-const CustomerEditForm = reduxForm({ form: 'CustomerEdit' })(CustomerEdit);
-
-export default connect(
-    (state, props) => (
-        { initialValues: props }
-    ))(CustomerEditForm); 
+            name: PropTypes.string,
+        dni: PropTypes.string,
+        age: PropTypes.number,
+    };
+    
+const CustomerEditForm = reduxForm({form: 'CustomerEdit' })(CustomerEdit);
+        
+export default setPropsAsInitial(CustomerEditForm);
