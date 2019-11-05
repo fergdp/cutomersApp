1ab6d9c4ae0f7e495f57235f5389d32e81098ec5
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2019-11-05 20:22:34 -0300

    195 - Validacion de fields a nivel global

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -3,14 +3,28 @@ import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 
+/*
 const isRequired = value => (
     !value && "Este campo es requerido"
-);
+); */
 
 const isNumber = value => (
     isNaN(Number(value)) && "El campo debe ser un numero"
 );
 
+const validate = values => {
+    const error = {};
+
+    if(!values.name) {
+        error.name = "El campo nombre es requerido";
+    }
+
+    if(!values.dni) {
+        error.dni = "El Dni es un campo obligatorio";
+    }
+
+    return error;
+}
 const MyField = ({ input, meta, type, label, name }) => (
     <div>
         <label htmlFor={name}>{label}</label>
@@ -29,13 +43,12 @@ const CustomerEdit = ({ name, dni, age }) => {
                 <Field
                     name="name"
                     component={MyField}
-                    validate={isRequired}
                     label="Nombre">
                 </Field>
                 <Field
                     name="dni"
                     component={MyField}
-                    validate={[isRequired, isNumber]}
+                    validate={[isNumber]}
                     label="Dni">
                 </Field>
                 <Field
@@ -56,6 +69,10 @@ CustomerEdit.propTypes = {
     age: PropTypes.number,
 };
 
-const CustomerEditForm = reduxForm({ form: 'CustomerEdit' })(CustomerEdit);
+const CustomerEditForm = reduxForm(
+    { 
+        form: 'CustomerEdit',
+        validate
+    })(CustomerEdit);
 
 export default setPropsAsInitial(CustomerEditForm);
