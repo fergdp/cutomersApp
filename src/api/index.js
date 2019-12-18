a1aa27d30be54335f69a8225b53e7144f3a40855
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2019-12-17 22:35:39 -0300

    InsertCustomer con POST

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,27 @@
 export const apiget = (url) => fetch(url).then(v => v.json());
 
-export const apiPut = (url, id, obj) => 
-    fetch(`${url}/${id}`,{
-        method:'PUT',
+export const apiPut = (url, id, obj) =>
+    fetch(`${url}/${id}`, {
+        method: 'PUT',
         body: JSON.stringify(obj),
-        headers: new Headers({'Content-type': 'application/json'})
+        headers: new Headers({ 'Content-type': 'application/json' })
     }).then(v => v.json())
-    .then(r => {
-        if(r.error) {
-            return Promise.reject(r.validation);
-        }
-        return r;
-    });
+        .then(r => {
+            if (r.error) {
+                return Promise.reject(r.validation);
+            }
+            return r;
+        });
+
+export const apiPost = (url, obj) =>
+    fetch(`${url}`, {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: new Headers({ 'Content-type': 'application/json' })
+    }).then(v => v.json())
+        .then(r => {
+            if (r.error) {
+                return Promise.reject(r.validation);
+            }
+            return r;
+        });
