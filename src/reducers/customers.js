9f505a79921e5b910be4e736c7fbb35613203988
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2019-12-18 19:12:50 -0300

    213 - ES6 Reduce sobre arrays (para update)

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -1,8 +1,28 @@
 import { handleActions } from 'redux-actions';
-import { FETCH_CUSTOMERS, INSERT_CUSTOMER } from '../constants/index';
+import { FETCH_CUSTOMERS, INSERT_CUSTOMER, UPDATE_CUSTOMER } from '../constants/index';
 
 export const customers = handleActions(
     {
         [FETCH_CUSTOMERS]: (state, action) => [...action.payload],
         [INSERT_CUSTOMER]: (state, action) => [...state, action.payload],
+        [UPDATE_CUSTOMER]: (state, action) => {
+            const customerPayload = action.payload;
+            const { id } = customerPayload; // i=2 name = 'nuevo nombre'
+            // [{id: 1, name: '', ...}
+            //  {id: 2, name: 'viejo nombre', ...}
+            //  {id: 3, name: '', ...}]
+            const customers = state; 
+            const initialvalue = [];
+
+            const newCustomers = customers.reduce((acc, customer) => {
+                if (customer.id === id){
+                    return [...acc, customerPayload];
+                } else {
+                    return [...acc, customer];
+                }
+            }, initialvalue);
+
+            return newCustomers
+
+        }
     }, []);
